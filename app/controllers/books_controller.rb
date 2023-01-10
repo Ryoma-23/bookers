2b0b99daaa407af9ce74c02cd1f500be678c0632
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-01-10 12:24:32 +0000

    [Add] バリデーションを設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,19 @@ class BooksController < ApplicationController
   def new
   end
   
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-  end
-
   def index
     @book = Book.new
     @books = Book.all
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
